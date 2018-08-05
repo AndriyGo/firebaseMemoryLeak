8871c43a5cdff23df720af7982c52936e1eb144c
Andriy Gordiychuk <andriy@glenmax.com>
2018-08-05 16:51:59 +0300

    Initial commit

---
+++ Podfile
@@ -0,0 +1,18 @@
+# Uncomment the next line to define a global platform for your project
+source 'https://github.com/CocoaPods/Specs.git'
+
+use_frameworks!
+
+def firebase
+    pod 'Firebase/Analytics'
+    pod 'Firebase/DynamicLinks'
+    pod 'Firebase/Core'
+end
+
+target 'firebaseMemoryLeak' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  platform :ios, '9.0'
+
+  firebase
+
+end
--- firebaseMemoryLeak/ViewController.swift
+++ firebaseMemoryLeak/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var triggerButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func pushNext(sender:UIButton) {
+        let viewC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "ViewController")
+        self.navigationController?.pushViewController(viewC, animated: true)
+    }
 
 }
 
